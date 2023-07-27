dd4f9beb9bf0104126382265a099aacfaad2806c
nickglu86 <enayfls@gmail.com>
2023-07-27 09:48:32 +0300

    fade in animations on DOM Load & scroll

--- main.js
+++ main.js
@@ -149,4 +149,40 @@ const tryAgain = () => {
 }
 
 const tryAgainBtn = document.querySelector('.form .error button');
-tryAgainBtn.addEventListener('click', tryAgain)
+tryAgainBtn.addEventListener('click', tryAgain);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.setTimeout(function() {
+    document.body.classList.remove('fade');
+  }, 230);
+});
+
+// const appear = document.querySelector('.appear'); 
+// const cb = function(entries){
+//   entries.forEach(entry => {
+//     if(entry.isIntersecting){
+//       entry.target.classList.add('inview');
+//     }else{
+//       entry.target.classList.remove('inview');
+//     }
+//   });
+// }
+// const io = new IntersectionObserver(cb);
+// io.observe(appear);
+
+const items = document.querySelectorAll('.appear');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview'); 
+        }else{
+            entry.target.classList.remove('inview'); 
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
