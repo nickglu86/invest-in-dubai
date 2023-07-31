492d6cf7a55959c65e64b108a2005627751bb830
nickglu86 <enayfls@gmail.com>
2023-07-31 07:42:23 +0300

    countrycode

--- main.js
+++ main.js
@@ -181,3 +181,9 @@ const io2 = new IntersectionObserver(active);
 for (let i = 0; i < items.length; i++) {
   io2.observe(items[i]);
 }
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
